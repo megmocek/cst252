cc9b8a7c8925c5b44c9d5dac3ba28f9b183284d5
megmocek <78106010+megmocek@users.noreply.github.com>
2021-02-08 15:37:08 -0800

    js comments & image

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// game.js
+  //1. START
+  //2. Name = INPUT: "Enter your name"
+      //a. IF name input is filled
+          //i. PRINT "Hello (name)"
+      //b. ELSE
+          //i. PRINT "Please enter your name"
+      //c. ENDIF
+  //3. Age = INPUT: "Enter your age"
+      //a. IF age input is filled
+          //i. PRINT "Your age is (age)"
+      //b. ELSE
+          //i. PRINT "Please enter your age"
+      //c. ENDIF
