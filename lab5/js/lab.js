b352c2e2fbec132d88a59de388ed0806e2139ce0
megmocek <78106010+megmocek@users.noreply.github.com>
2021-02-10 14:31:55 -0800

    Lab 5 Output

---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+// lab.js
+// Use .js to play with variables and stuff
+// Author: Meg Mocek
+// Created: Feb. 10, 2021
+// License: Public Domain
+
+
+console.log("Starting JavaScript")
+
+//Declare variables to hold the make, model, color, and year of your vehicle.
+var make = "Honda";
+var model = "Civic";
+var color = "Silver";
+var year = 2008;
+
+//Declare the boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+
+//Create a new variable age that subtracts the year from the current year.
+age = 2021 - year;
+
+//Use document.writeln() to neatly output each of your variables.
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
