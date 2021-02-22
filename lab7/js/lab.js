208fd620b7dc64a09aed7ac38beeb7639ab6896e
megmocek <78106010+megmocek@users.noreply.github.com>
2021-02-22 12:27:33 -0800

    js update

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,3 +3,23 @@
 // Author: Meg Mocek
 // Created: Feb. 22, 2021
 // License: Public Domain
+
+//sortUserName
+Function sortUserName() {
+  var userName = window.prompt("Hi, please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //Split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //Sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //Join array back to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//OutPut
+document.writeln("Hey, I fixed your name: "),
+        sortUserName(), "</br>";
