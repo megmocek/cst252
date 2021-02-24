e0f40c27ca84b7667e4151f1dd45bd2be3186855
megmocek <78106010+megmocek@users.noreply.github.com>
2021-02-24 11:04:26 -0800

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,5 +1,5 @@
 // lab.js
-// Use .js to play with anon functions & playbacks
+// Use .js to play with anon functions & callbacks
 // Author: Meg Mocek
 // Created: Feb. 24, 2021
 // License: Public Domain
