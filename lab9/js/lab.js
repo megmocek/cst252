dfcda942b44896dcd45916a8e3ce425b7682f8e7
megmocek <78106010+megmocek@users.noreply.github.com>
2021-03-01 14:34:24 -0800

    Java

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,3 +3,12 @@
 // Author: Meg Mocek
 // Created: Mar. 1, 2021
 // License: Public Domain
+
+oldElement = document.getElementById("img");
+newElement = document.createElement("div");
+oldElement.appendChild(newElement);
+newElement.id = "Gimli";
+newElement.innerHTML = "My cat is cute.";
+newElement.style.fontSize = "30pt";
+newElement.style.fontFamily = 'Courier New';
+newElement.style.color = "#8A0A23";
