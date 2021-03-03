f1befd3fe98586cf27aa7319c4bcf8e56e29f77d
megmocek <78106010+megmocek@users.noreply.github.com>
2021-03-03 12:32:53 -0800

    js fix

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,18 +5,12 @@
 // License: Public Domain
 
 //In class code with Wes
-var titleEl = document.getElementById("user-name");
-console.log("Title: ", titleEl);
-
-var buttonEl = document.getElementById("mybutton");
-console.log("Button", buttonEl);
-
-var inputEl = document.getElementById("my-input");
-console.log("Input Field", inputEl);
+var buttonEl = document.getElementById("my-button");
+console.log("El Button", buttonEl);
 
 buttonEl.addEventListener("click", function(){
-  var name = inputEl.value;
-  //var name = prompt("What's your name?");
+  var name = prompt("What's your name?");
+  var titleEl = document.getElementById("user-name");
+  console.log("Title element: ", titleEl);
   titleEl.innerText = "Hello, " + name;
-  inputEl.value = "";
 })
