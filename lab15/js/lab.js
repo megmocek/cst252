8c52764f6eee81ac55fa751e971ecb4279bc8ae6
megmocek <78106010+megmocek@users.noreply.github.com>
2021-03-24 10:58:25 -0700

    lab 15 push

---
+++ lab15/js/lab.js
@@ -0,0 +1,7 @@
+// lab.js
+// Use .js to make Job-Oriented Design
+// Author: Meg Mocek
+// Created: Mar. 24, 2021
+// License: Public Domain
+
+//I hope I did this right because I tried to follow along with Wes in class.
