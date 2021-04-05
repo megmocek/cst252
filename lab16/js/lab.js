3722ccc982d23d3fbddb16583c59af13d754ba29
megmocek <78106010+megmocek@users.noreply.github.com>
2021-04-05 11:24:11 -0700

    lab16

---
+++ lab16/js/lab.js
@@ -0,0 +1,15 @@
+// lab.js
+// Use .js to make JavaScript JQuery
+// Author: Meg Mocek
+// Created: Apr. 5, 2021
+// License: Public Domain
+
+button - $("output").append("<button id-button class-button> A button");
+
+but = $("#button");
+
+$(".button").css("color", "red");
+
+but.click(function() {
+    alert("Your fortune is... DEATH BY COVID.");
+});
