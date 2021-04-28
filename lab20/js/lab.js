4516d88e8c47271391e90c879417cf1587851715
megmocek <78106010+megmocek@users.noreply.github.com>
2021-04-28 10:47:38 -0700

    lab 20 adjust

--- lab20/js/lab.js
+++ lab20/js/lab.js
@@ -18,6 +18,7 @@ $("#activate").click(function(){
   })
 });
 
+// Wes' in class
 // // var myObject = { "id": "watermelon",
 // //   "available": true,
 // //   "name": "Watermelon",
@@ -37,3 +38,60 @@ $("#activate").click(function(){
 //         $(".biography").append(newImage);
 //     }
 // })
+
+// Wes' code
+// var URL = "https://xkcd.com/info.0.json";
+//
+// var URLpre = "https://xkcd.com/";
+// var URLpost = "info.0.json";
+//
+// // Endpoint format: http://xkcd.com/614/info.0.json
+//
+// function getComic(num) {
+//   if (typeof num === 'undefined') {
+//     numStr = "";
+//   } else {
+//     numStr = num.toString() + "/";
+//   }
+//   var ourURL = URLpre + numStr + URLpost;
+//   // get data via ajax from numbersapi
+//   // Using the core $.ajax() method
+//   $.ajax({
+//       // The URL for the request (ENDPOINT)
+//       url: ourURL,
+//       // Whether this is a POST or GET request
+//       type: "GET",
+//   })
+//   // If the request succeeds
+//   .done(function(data) {
+//       // console.log(data);
+//       var imageUrl = data.img;
+//       var title = data.title;
+//       var alt = data.alt;
+//       var comicNum = data.num;
+//       var html = `<div id="imageblock">
+//           <h2>${title}</h2>
+//           <img src="${imageUrl}" title="${alt}"><br>
+//           <button id="prev">Previous</button><button id="next">Next</button>
+//         </div>`
+//       console.log("My new html: ", html);
+//       $("#output").html(html);
+//
+//       // add event listener to new prev button
+//       $("#prev").click(function(){
+//         getComic(comicNum - 1);
+//       });
+//       // add event listener to new next button
+//       $("#next").click(function(){
+//         getComic(comicNum + 1);
+//       });
+//   })
+//   .fail(function(){
+//     console.log("^^ Please ignore this error. It's okay.");
+//     console.log("Have a happy day! :-)");
+//   })
+//
+// }
+//
+// // start things off
+// getComic();
